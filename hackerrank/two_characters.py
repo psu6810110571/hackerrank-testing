25cdf8c0376ca934286fcde3d316a460e1555b61
psu6810110571 <6810110571@psu.ac.th>
2026-03-05 20:39:53 +0700

    chore: เพิ่มโค้ดตั้งต้นจาก HackerRank สำหรับโจทย์ Two Characters

---
+++ hackerrank/two_characters.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternate' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternate(s):
+    # Write your code here
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    s = input()
+
+    result = alternate(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
