5f97e0d45cdd462de2911b7fc5bb890dd0fdb6a6
psu6810110571 <6810110571@psu.ac.th>
2026-03-06 17:38:59 +0700

    test: เพิ่ม Test Case ที่ 2 สำหรับ Staircase (กรณีบันได 4 ขั้นตามสไลด์)

--- tests/test_staircase.py
+++ tests/test_staircase.py
@@ -16,3 +16,19 @@ class TestStaircase(unittest.TestCase):
         
         # Assert
         self.assertEqual(result, expected_output, f'Should be {expected_output}')
+
+    def test_give_4_with_hash_should_return_staircase(self):
+        # Arrange: กรณีบันได 4 ขั้น ตามสไลด์หน้า 13
+        n = 4
+        pattern = '#'
+        expected_output = \
+        "   #\n" + \
+        "  ##\n" + \
+        " ###\n" + \
+        "####"
+        
+        # Act
+        result = staircase(n, pattern)
+        
+        # Assert
+        self.assertEqual(result, expected_output)
